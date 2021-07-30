a7a0df7207038991aca80fcc5cadaa640b077359
EsatYasar <esatyasar91@gmail.com>
2021-07-30 14:04:38 +0300

    refresh button added

--- generator-password/script.js
+++ generator-password/script.js
@@ -1,5 +1,4 @@
 // variables
-
 const length = document.querySelector("#length")
 const upper = document.querySelector("#uppercase")
 const lower = document.querySelector("#lowercase")
@@ -7,97 +6,82 @@ const num = document.querySelector("#number")
 const sym = document.querySelector("#symbol")
 const submit = document.querySelector(".generator")
 const result = document.querySelector("#password")
-
-
+// strings
 const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*()_+=";
 
-
 function getUppercase() {
     return upperLetters[Math.floor(Math.random() * upperLetters.length)];
 }
-
 function getLowercase() {
     return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
 }
-
 function getNumber() {
     return numbers[Math.floor(Math.random() * numbers.length)];
 }
-
 function getSymbol() {
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
 var password = [];
-
 upper.addEventListener("change", () =>{
     if(upper.checked){
-        
         generateUpper(password)
-        
     }
-    
 })
-
 lower.addEventListener("change", () =>{
     if(lower.checked){
-        
         generateLower(password)
-        
     }
-    
 })
 num.addEventListener("change", () =>{
     if(num.checked){
-        
         generateNum(password)
-        
     }
-    
 })
-
 sym.addEventListener("change", () =>{
     if(sym.checked){
-        
         generateSym(password)
-        
     }
-    
 })
-
 function generateUpper(password){
     for (let i = 0; i < length.value; i++){
         password.push(getUppercase());    
     }
 }
-
 function generateLower(password){
     for (let i = 0; i < length.value; i++){
         password.push(getLowercase());    
     }
 }
-
 function generateNum(password){
     for (let i = 0; i < length.value; i++){
         password.push(getNumber());    
     }
 }
-
 function generateSym(password){
     for (let i = 0; i < length.value; i++){
         password.push(getSymbol());    
     }
 }
 
-submit.addEventListener("click", () =>{
 
+submit.addEventListener("click", () =>{
     var newPassword = ""
     for (let i = 0; i < password.length; i++) {
-        newPassword += password[Math.floor(Math.random()* password.length)]
+         var x = password[Math.floor(Math.random()* password.length)]
+         if(newPassword.includes(x)){
+             i--
+         }else{
+            newPassword += x
+         }  
     }
-  
     result.innerHTML = newPassword;
+    submit.textContent = "Refresh" ;
+    submit.addEventListener("click", () => {
+        if (result.innerHTML !== ""){
+            window.location.reload()
+        }   
+    })
 })
-
